9987f5bfca56ba4fecbb8a832c2ba2aa04a77fc7
stanly-sudo <alys.git24@gmail.com>
2025-10-12 07:32:20 +0800

    Introduction to Data Science: (module 1) data usage in daily life and practices.

---
+++ DICT/Introduction to Data Science/module 1/1.1.3/data usage in daily life.py
@@ -0,0 +1,53 @@
+
+#! DATA USAGE IN DAILY LIFE:
+# data is being generated from censors, devices, video, audio, networks, log files, transactional
+# applications, the web and social media.
+
+
+
+#! USES OF DATA IN OUR DAILY LIVES:
+
+#* INFORMING DECISION MAKING:
+# from the product recommendations we see on shopping sites to performance statistics on athletes
+# to which groceries stocking the market, our decisions are influence of data and ai.
+
+
+#* IMPROVEMENTS to PROCESS and PRODUCTS:
+# through the ability to use data collective from product reviews and ratings, manufacturers can
+# modify and change their processes to produce and deliver better products to their consumers.
+
+
+#* TRACKING and PREDICTING EVENTS:
+# one of the best abilities to tracking and predicting events is a network engineer can use the
+# dashboard to isolate areas of slowdown and immidiately react and ensure it does not cause
+# any problems for users, climate analysts can accurately predict the impacts of weather in all
+# aspects of our economics, self-driving cars can recognize hazards and take actions to avoid
+# accidents.
+
+
+#* BEHAVIORAL ANALYSIS:
+# organizations can have greater visibility with how their products are used, the buying patterns
+# of their customers and the up-to-date logistical information and their lead times, this level
+# of additional visibility enables managers to ensure that the right products are available for
+# thier customers in the shortest time possible.
+
+
+#! 1.1.4 PRACTICE  (Data Usage in Daily Life):
+# Data crunchers has a number of their data and use it to identify trends, make decisions, and predict
+# future conditions.
+
+#* 1. Make Informed decisions
+# why? becase "an online shopping site analyzes which products have the longest lead times to choose
+# which products to keep in inventory."
+
+#* 2. Identify Improvements to Processeses and products
+# why? because an automobile manufacturer uses automobile repair records and customer feedback to add
+# additional vehicle inspections to the production line.
+
+#* 3. Enable Greater Visibility into Custoer Behaviors
+# why? because when a mobile phone manufacturer uses data about phone product types, sources of purchase
+# and customer use patterns to better provide them with the products they want.
+
+#* 4. Track and Predict Events
+# why? because when a climate analyst uses data from previous years to forecast hurricane activity in the
+# upcoming season.
