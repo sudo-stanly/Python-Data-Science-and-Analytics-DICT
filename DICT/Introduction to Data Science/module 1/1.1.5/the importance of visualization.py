472fe9d308e6994a09ab9b90f4951480ee0b9cda
stanly-sudo <alys.git24@gmail.com>
2025-10-12 09:10:11 +0800

    Introduction to Data Science: (module 1) the importance of visualization.

---
+++ DICT/Introduction to Data Science/module 1/1.1.5/the importance of visualization.py
@@ -0,0 +1,22 @@
+
+#! IMPORTANCE of VISUALIZATION
+# the data analysts at data crunchers work hard to create stories and reports from
+# their clients' data. analysts rey heavily on visualization techniques to make the
+# information relevant and actionable.
+# visualizations are a way to display data in a manner that is easily understood. the
+# most common ways to represent data visually are charts and graphs.
+
+# there are many types of charts used to present information graphically. it is crucial
+# to ensure that the type of visualization used best illusstrates the pattern, problem,
+# or trend that needs highlighting; not all types of visualizations are appropriate for
+# all kinds of data.
+
+
+#! Factors to consider when choosing a visualization:
+#  - The number of variables, which are the characteristics measured, that need are shown.
+#  - The number of data points, or units of information, in each variable.
+#  - Wheter the data  illustrates changes over time (hour, daily, weekly)
+#  - The need to make a comparison or correlation between different data points
+
+# often, analysts try a few different types of charts to choose one that provides the most
+# effective view of the information.
