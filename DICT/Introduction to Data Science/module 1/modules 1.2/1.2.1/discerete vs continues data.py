7b7c4bb038a906db81c3efd3fd5736f36971aa45
stanly-sudo <alys.git24@gmail.com>
2025-10-13 14:42:55 +0800

    Introduction to Data Science: (module 1) discrete vs continuous data.

---
+++ DICT/Introduction to Data Science/module 1/modules 1.2/1.2.1/discerete vs continues data.py
@@ -0,0 +1,28 @@
+
+#* data analysis begins with types of data:
+# data can first be defined as qualitative or quantitative data.
+
+#* QUALITATIVE DATA:
+# - is descriptive
+# - it is typically described as text
+# - non numerical in nature
+# - collective questionares, interviews, or observations.
+
+#! example of qualitative data:
+# - reviews about the products on a shopping site (customer reviews)
+
+#* QUANTITATIVE DATA:
+# - numerical in nature
+# - quantifying information (discrete or continous)
+# (discrete data) can only take certain values, typically whole numbers
+
+# ! example of discrete data:
+# - account transactions, process n a day, or a number of people who visit a website
+#   each week.
+
+#* continuos data:
+# - is measured and can be represented infinite number and ways
+
+#! example of continuos data:
+# - average disease travelled by a moving car
+#   which constantly updates as the car moves forward.
