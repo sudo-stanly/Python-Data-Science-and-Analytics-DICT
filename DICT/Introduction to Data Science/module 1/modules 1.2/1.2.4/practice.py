ed034378e1d10701402cb3a6318a603b68c24a81
stanly-sudo <alys.git24@gmail.com>
2025-10-13 23:12:40 +0800

    Introduction to Data Science: (module 1) data type practice.

---
+++ DICT/Introduction to Data Science/module 1/modules 1.2/1.2.4/practice.py
@@ -0,0 +1,34 @@
+
+#! EMPLOYEE INFORMATION:
+# refer to the employee information table below. you are tasked with defining the correct
+# data types that can be stored each of the columns.
+
+#* for each column name in the employee information table, select the correct data type.
+#* each data type can be used more than once.
+
+#! 1. Name
+#* answer: 
+
+#! 2. Employee ID
+#* answer: string
+
+#! 3. Employee Date
+#* answer: date
+
+#! 4. Job Title
+#* answer: string
+
+#! 5. Base City
+#* answer: string
+
+#! 6. Average Weekly Hourse
+#* answer: floating point
+
+#! 7. Contract
+#* answer: boolean
+
+#! 8. Yearly Vacation Days
+#* answer: integer
+
+#! 9. Gym Membership
+#* answer: boolean
