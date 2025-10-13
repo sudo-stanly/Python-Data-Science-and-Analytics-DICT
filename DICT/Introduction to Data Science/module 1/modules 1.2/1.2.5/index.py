838508f3177f7c0333cd5d89cdf1fc108c33c24f
stanly-sudo <alys.git24@gmail.com>
2025-10-13 23:22:38 +0800

    Introduction to Data Science: (module 1) variety of data.

---
+++ DICT/Introduction to Data Science/module 1/modules 1.2/1.2.5/index.py
@@ -0,0 +1,10 @@
+
+#! 1.2.5 VARIETY of DATA:
+# data is all around us, and it is coming at ever-increasing rates and in many
+# different formats. for example, when mobile phones first became popular, they
+# had only a few megabytes of data storage and were limited to the variety of
+# data they could store. these early mobile phones, used primarily for phone calls,
+# could keep text messages, a couple of MP3 music files, and a few low-resolution photos.
+# modern-day mobile phones come with up to a terabyte (1000 gigabytes) of storage. they
+# can store growing variety of data types, including high-definition video files, photographs,
+# audio files, documents, apps, books, calendars, and emails, to name a few.
