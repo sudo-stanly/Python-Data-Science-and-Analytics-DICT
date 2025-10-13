a0eeba3cd379fbae038c6762a5fc76730d55360e
stanly-sudo <alys.git24@gmail.com>
2025-10-13 15:06:00 +0800

    Introduction to Data Science: (module 1) data types.

---
+++ DICT/Introduction to Data Science/module 1/modules 1.2/1.2.3/data types.py
@@ -0,0 +1,13 @@
+
+#! 1.2.3 DATA TYPES:
+# all pieces of data have a feature called data type that tells applications
+# how to treat it. operations performed are defined by the data type. identifying
+# data types is helpful in analytics because we may need to group the data, sort
+# the data, or run calculations on the data. data must be grouped by their given type to perform
+# required operations. for example: consider the following data crunchers employee table.
+
+
+# Each column (or field) in the Employee Information table has data that is all the same type. 
+# The next table  describes the various data types. Read the descriptions for each data type. 
+# Compare them to the data displayed in the Employee Information table and think about which 
+# type each field is (For example, Job Title contains string data). 
