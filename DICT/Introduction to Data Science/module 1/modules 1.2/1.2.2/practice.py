28c9b7a3c8b2cdfd99f7309b4aa29fdf1311ecae
stanly-sudo <alys.git24@gmail.com>
2025-10-13 14:52:20 +0800

    Introduction to Data Science: (module 1) discrete vs continous data practice.

---
+++ DICT/Introduction to Data Science/module 1/modules 1.2/1.2.2/practice.py
@@ -0,0 +1,22 @@
+
+#! PRACTICE ITEM DISCRETE VS CONTINUOS DATA:
+# our bicycle store client is collecting data for analysis. for each example,
+# choose if discrete data or continuous data.
+
+#! 1. the number of employees in each department.
+#* answer: discrete
+
+#! 2. the temperature inside the store.
+#* answer: continuos
+
+#! 3. the distance traveled by a moving bicycle.
+#* continuos
+
+#! 4. the number of customers who bought bicycles this month.
+#* discrete
+
+#! 5. the speed of a rider in a bike race.
+#* continous
+
+#! 6. the amount of diesel fuel purchased each week for a delivery truck.
+#* discrete
