402fdf085e0664813d68377501da0c0f6837b529
stanly-sudo <alys.git24@gmail.com>
2025-10-15 07:02:02 +0800

    Introduction to Data Science: module 1.3 folder.

---
+++ DICT/Introduction to Data Science/module 1/modules 1.2/1.2.8/practice.py
@@ -0,0 +1,40 @@
+
+#! 1.2.9 PRACTICE ITEM - SELECTING RELEVANT DATA
+# employee information
+# refer to the employee information table below. one of data crunchers clients
+# is a large national firm with multiple branch office locations. you are gathering
+# data to help prepare a report regarding general workforce wellness. the report
+# will be presented at the yearly meeting with branch managers and needs to ahve
+# the wellness data broken down by branch office.
+
+#* note: base city has been changed to branch office.
+
+# Select whether the data in a column in the Employee Information table is relevant 
+# or not relevant to the workforce wellness report. 
+
+#! name
+#* answer: not relevant
+
+#! employee id
+#* answer: not relevant
+
+#! employee date
+#* answer: not relevant
+
+#! job title
+#* answer: not relevant
+
+#! branch office
+#* answer: relevant
+
+#! average weekly hours
+#* answer: not relevant
+
+#! contract
+#* answer: not relevant
+
+#! yearly vacation days
+#* answer: not relevant
+
+#! gym membership
+#* answer: relevant
