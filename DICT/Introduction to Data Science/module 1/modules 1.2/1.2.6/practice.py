03da42e319400adc98f239f3ba973b8b85bb1c56
stanly-sudo <alys.git24@gmail.com>
2025-10-14 09:50:09 +0800

    Introduction to Data Science: (module 1) structured vs unstructured data practice.

---
+++ DICT/Introduction to Data Science/module 1/modules 1.2/1.2.6/practice.py
@@ -0,0 +1,24 @@
+
+#! 1.2.7 PRACTICE ITEM - STRUCTURED vs UNSTRUCTURED DATA
+# choose if data is represented as structured data or unstructured data.
+
+#! spreadsheets
+#* answer: structured
+
+#! social media posts
+#* answer: unstructured data
+
+#! sales transactions
+#* answer: structured data
+
+#! surveillance video
+#* answer: unstructured data
+
+#! relational databases
+#* answer: structured data
+
+#! credit card numbers
+#* structured data
+
+#! email messages
+#* unstructured data
