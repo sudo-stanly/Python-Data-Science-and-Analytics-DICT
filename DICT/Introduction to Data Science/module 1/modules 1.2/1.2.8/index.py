e7ee47ab6a30d1bc8a1f0912e93a400964643ebf
stanly-sudo <alys.git24@gmail.com>
2025-10-14 10:10:53 +0800

    Introduction to Data Science: (module 1) selecting relevant data.

---
+++ DICT/Introduction to Data Science/module 1/modules 1.2/1.2.8/index.py
@@ -0,0 +1,9 @@
+
+#! 1.2.8 SELECTING RELEVANT DATA
+# selecting relevant data starts with defining the question that you want to answer.
+# for example, suppose you are researching the factors that contribute to the overall
+# wellness of the workforce. you can start by looking at the available data and determining
+# which fields or measurements are relevant to your question. in the employee information table,
+# repeated below, employment date has no impact on employee wellness therefore is irrelevant to
+# the analysis. in contrast, overwork could impact employee wellness, and therefore, average
+# weekly hours could be relevant to the search.
