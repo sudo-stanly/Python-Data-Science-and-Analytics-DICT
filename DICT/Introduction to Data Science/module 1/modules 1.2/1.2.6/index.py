a34d8232acd986e6d7406390c86f2503469646da
stanly-sudo <alys.git24@gmail.com>
2025-10-14 09:32:58 +0800

    Introduction to Data Science: (module 1) structured vs unstructured data.

---
+++ DICT/Introduction to Data Science/module 1/modules 1.2/1.2.6/index.py
@@ -0,0 +1,20 @@
+
+#! 1.2.6 STRUCTURED vs UNSTRUCTED DATA
+# in order to process, store, and analyze all of these different types of data,
+# it is important to think about whether they are structured data or unstructured
+# data.
+
+#* strucuted data:
+# structured data makes up about 10%-20% of generated data has clearly defined data types
+# and patterns that make them easily to search and analyze. sources of structured data
+# include sales records, airline reservation systems, and inventory control, structured
+# data is usually stored in relational databases such as structured query language (sql)
+# databases or in spreadsheets such as microsoft excel.
+
+
+#* unstructured data:
+# unstructured data makes up most data that is generated, about 80%, and cannot be organized
+# into row and columns. this makes unstructured data difficult to search, manage, and analyze.
+# sources of unstructured data include images, pdfs, sensor data, and social media posts,
+# unstructured data is usually stored in a non-relational database also known as NoSQL database
+
